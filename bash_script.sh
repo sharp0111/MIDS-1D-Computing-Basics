e891b5f67344c71d5ffd37f2c62dbcfbee6546c5
Sergey Nam <shaqk702@gmail.com>
2023-12-23 13:15:56 -0800

    Rename index.html, fix reference to array file items

--- bash_script.sh
+++ bash_script.sh
@@ -2,11 +2,11 @@
 
 # Author: Sergey Nam
 
-# Create an empty new_index.html file
-touch new_index.html
+# Create an empty index.html file
+touch index.html
 
-# Copy first 19 lines from original index.html into new_index.html
-sed -n "1,14p"  ./website/index.html > new_index.html
+# Copy first 19 lines from original index.html into the new index.html
+sed -n "1,14p"  ./website/index.html > index.html
 
 # Create a variable for images directory
 IMAGES_DIR="./website/images"
@@ -16,8 +16,6 @@ i=0
 for f in "$IMAGES_DIR"/*;
 do
   FILES[$i]="$f"
-  # Echo array item
-  echo "${FILES[$i]}"
   i=`expr $i + 1`
 done
 
@@ -31,17 +29,17 @@ row=0
 col=0
 while [ "$row" -lt "$NUMBER_OF_ROWS" ]
 do
-   echo '    <tr>' >> new_index.html
-   echo '      <td><img src=${FILES[$col]} alt="" border=3 height=100 width=300></th>' >> new_index.html
+   echo "    <tr>" >> index.html
+   echo "      <td><img src=\"${FILES[$col]}\" alt=\"\" border=3 height=100 width=300></th>" >> index.html
    col2=`expr $col + 1`
-   echo '      <td><img src=${FILES[$col2]} alt="" border=3 height=100 width=300></th>' >> new_index.html
+   echo "      <td><img src=\"${FILES[$col2]}\" alt=\"\" border=3 height=100 width=300></th>" >> index.html
    col3=`expr $col + 2`
-   echo '      <td><img src=${FILES[$col3]} alt="" border=3 height=100 width=300></th>' >> new_index.html
-   echo '    </tr>' >> new_index.html
+   echo "      <td><img src=\"${FILES[$col3]}\" alt=\"\" border=3 height=100 width=300></th>" >> index.html
+   echo "    </tr>" >> index.html
    
    row=`expr $row + 1`
    col=`expr $row \* 3`
 done
 
-# Append the last 25-27 lines to the new_index.html
-sed -n "25,27p" ./website/index.html >> new_index.html
+# Append the last 25-27 lines to the new index.html
+sed -n "25,27p" ./website/index.html >> index.html
