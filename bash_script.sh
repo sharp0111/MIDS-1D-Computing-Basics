ee5bf15e0fbec4b8bcfb75012e0cb1d4e314335a
Sergey Nam <shaqk702@gmail.com>
2023-12-22 22:46:06 -0800

    Refactor loop, start using array for file names

--- bash_script.sh
+++ bash_script.sh
@@ -8,30 +8,39 @@ touch new_index.html
 # Copy first 19 lines from original index.html into new_index.html
 sed -n "1,14p"  ./website/index.html > new_index.html
 
-# Create a filelist.txt for file names
-touch filelist.txt
-
 # Create a variable for images directory
 IMAGES_DIR="./website/images"
 
 # Run a loop for file names
-for f in "$IMAGES_DIR"/*; do
-  echo "$f" >> filelist.txt
+i=0
+for f in "$IMAGES_DIR"/*;
+do
+  FILES[$i]="$f"
+  # Echo array item
+  echo "${FILES[$i]}"
+  i=`expr $i + 1`
 done
 
 NUMBER_OF_IMAGES=159
 THREE=3
 # Get number of images per row
-# NUMBER_OF_ROWS=expr $NUMBER_OF_IMAGES/$THREE
-# echo $NUMBER_OF_ROWS
+NUMBER_OF_ROWS=`expr $NUMBER_OF_IMAGES / $THREE`
 
 # Append table rows
-for i in {1..53}; do
-  echo "    <tr>" >> new_index.html
-  echo "      <td><img src=\"./website/images/CircleGradient2GoldenRectangle.png\" alt=\"\" border=3 height=100 width=300></th>" >> new_index.html
-  echo "      <td><img src=\"./website/images/Gráfico de dos funciones 01.jpg\" alt=\"\" border=3 height=100 width=300></th>" >> new_index.html
-  echo "      <td><img src=\"./website/images/Calculus- the graph of y = 4-5x^3 - 2x^2 + 1.png\" alt=\"\" border=3 height=100 width=300></th>" >> new_index.html 
-  echo "    </tr>" >> new_index.html
+row=0
+col=0
+while [ "$row" -lt "$NUMBER_OF_ROWS" ]
+do
+   echo '    <tr>' >> new_index.html
+   echo '      <td><img src=${FILES[$col]} alt="" border=3 height=100 width=300></th>' >> new_index.html
+   col2=`expr $col + 1`
+   echo '      <td><img src=${FILES[$col2]} alt="" border=3 height=100 width=300></th>' >> new_index.html
+   col3=`expr $col + 2`
+   echo '      <td><img src=${FILES[$col3]} alt="" border=3 height=100 width=300></th>' >> new_index.html
+   echo '    </tr>' >> new_index.html
+   
+   row=`expr $row + 1`
+   col=`expr $row \* 3`
 done
 
 # Append the last 25-27 lines to the new_index.html
